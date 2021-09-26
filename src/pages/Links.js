64916a17945a5afbe10463ad4c85e893ca588eac
Gokalp Gursoy <gkalpgursoy@gmail.com>
2021-09-26 16:18:23 +0300

    styled-component is added

--- src/pages/Links.js
+++ src/pages/Links.js
@@ -1,5 +1,17 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: red;
+`;
+
 function Links() {
-  return <h1>Link List</h1>;
+  return (
+    <Wrapper>
+      <h1>Link List</h1>
+    </Wrapper>
+  );
 }
 
 export default Links;
