742a380d45ac429b83b0f1b21ba48dbc874e46e3
Gokalp Gursoy <gkalpgursoy@gmail.com>
2021-09-27 20:01:06 +0300

    Layout of Links pAGE İS CREATED

--- src/pages/Links.js
+++ src/pages/Links.js
@@ -1,7 +1,32 @@
+import styled from 'styled-components';
 import SubmitLink from '../components/Links/SubmitLink';
 
+const Seperator = styled.div`
+  width: 100%;
+  height: 5px;
+  background-color: #ececec;
+  margin: 20px 0;
+`;
+
+const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const PageContainer = styled.div`
+  padding: 0 15px;
+`;
+
 function Links() {
-  return <SubmitLink />;
+  return (
+    <PageWrapper>
+      <PageContainer>
+        <SubmitLink />
+      </PageContainer>
+      <Seperator />
+    </PageWrapper>
+  );
 }
 
 export default Links;
