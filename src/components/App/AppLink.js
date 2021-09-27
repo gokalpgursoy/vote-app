85c81801a35f2864d5e1c337959e37e1f962b6e0
Gokalp Gursoy <gkalpgursoy@gmail.com>
2021-09-28 01:36:01 +0300

    AppLink is fixed

--- src/components/App/AppLink.js
+++ src/components/App/AppLink.js
@@ -1,20 +1,23 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const LinkWrapper = styled.div`
+const LinkWrapper = styled(Link)`
   display: flex;
   align-items: center;
   justify-content: flex-start;
   width: 100%;
-  gap: 5px;
+`;
+
+const Text = styled.span`
   font-weight: 500;
+  margin: 0 5px;
 `;
 
 function AppLink({ text, href, children }) {
   return (
-    <LinkWrapper>
+    <LinkWrapper to={href}>
       {children}
-      <Link to={href}>{text}</Link>
+      <Text>{text}</Text>
     </LinkWrapper>
   );
 }
