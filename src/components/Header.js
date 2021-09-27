6d03cede06da1b72150d136da792584102b7bcdf
Gokalp Gursoy <gkalpgursoy@gmail.com>
2021-09-28 02:09:50 +0300

    Header is completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,38 @@ const HeaderWrapper = styled.header`
   border-bottom: 1px solid black;
   flex: 0 0 60px;
   width: 100%;
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+`;
+
+const Brand = styled.h2`
+  text-transform: lowercase;
+  font-size: 24px;
+`;
+
+const Title = styled.h2`
+  display: inline-block;
+  font-size: 24px;
+  font-weight: 800;
+`;
+
+const SubTitle = styled.h2`
+  display: inline-block;
+  font-size: 24px;
+  font-weight: 200;
 `;
 
 function Header() {
-  return <HeaderWrapper>Header</HeaderWrapper>;
+  return (
+    <HeaderWrapper>
+      <Brand>Brand</Brand>
+      <div>
+        <Title>Link</Title>
+        <SubTitle>VOTE Challenge</SubTitle>
+      </div>
+    </HeaderWrapper>
+  );
 }
 
 export default Header;
